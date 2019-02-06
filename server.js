57a16aa51b23cc52e6a690f7369cdca4c5139e65
MusaAliJ <imusajahangiri@gmail.com>
2019-02-06 20:13:17 +0500

    Set up routes for app

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
 const app = express();
 
 
@@ -11,6 +16,12 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
 .then(()=>console.log('Database Has Connected'))
 .catch(err=>console.log('Database has some problem in connection',err))
+
+
+
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+app.use('/api/users', users);
 
 
 app.get('/',(req,res)=>res.send('Hello Alpha!'));
